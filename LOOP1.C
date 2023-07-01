ce18e26b5b83b5e975296667644c0506a05afa3e
sidhant112233 <137783068+sidhant112233@users.noreply.github.com>
2023-07-01 15:54:06 +0530

    Add files via upload

---
+++ LOOP1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{        int i=1,n;
+
+	clrscr();
+	while(i<=10)
+	{
+		printf("%d \n",i);
+		i++;
+	}
+	getch();
+	}
+
+
+
---
+++ LOOP3.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{        int i=1,n;
+	 clrscr();
+
+	/* printf("Enter a value :");
+	 scanf("%d",&i);*/
+
+	 printf("Enter a value :");
+	 scanf("%d",&n);
+
+	 while(i<=n)
+	 {
+		printf("%d\t",i);
+		i++;
+	 }
+	 getch();
+}
